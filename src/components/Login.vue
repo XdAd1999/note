0e1b3da4f893e7c5cf382a27ee76ae4df68e3f48
阿栋的仓库 <2675413003@qq.com>
2021-01-06 14:14:12 +0800

    ‘’

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<h1>router</h1>
 </template>
 
 <script>
 export default {
-  name: 'App'
+name: "Login"
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from "../components/Login";
+import NoteBooks from "../components/NoteBookList";
+import Note from "../components/NoteDetail";
+import Trash from "../components/TrashDetail";
+
 
 Vue.use(Router)
 
@@ -8,8 +12,24 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      component: Login
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: NoteBooks
+    },
+    {
+      path: '/note/:noteId',
+      component: Note
+    },
+    {
+      path: '/trash/:noteId',
+      component: Trash
+    },
+
   ]
 })
